dc839de410605092bc75550e5c4d1dc30f4ed2dc
Alex T <aletapia@proton.me>
2022-11-30 20:29:30 -0600

    added method to separate newly created token assets from duplicates to take appropriate actions

--- packages/nest-server/src/nft/nft.controller.ts
+++ packages/nest-server/src/nft/nft.controller.ts
@@ -13,6 +13,7 @@ import {
 import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express'
 import { Prisma } from '@prisma/client'
 import { parse } from 'csv/sync'
+import { findIndex, partition } from 'lodash'
 import { z } from 'nestjs-zod/z'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserOwnsCollection } from 'src/collection/user-owns-collection.guard'
@@ -107,12 +108,26 @@ export class NftController {
     assets: Express.Multer.File[],
     @Param('collectionId', ParseIntPipe) collectionId: number,
   ) {
-    const originalAssetNames = assets.map((a) => a.originalname.split('.')[0])
-    if (!this.utilService.isArrayInSequence(originalAssetNames))
+    const requestAssetIds = assets.map((a) => a.originalname.split('.')[0])
+    if (!this.utilService.isArrayInSequence(requestAssetIds))
       throw new BadRequestException('assets are not in sequence')
-    // does the collection already has tokenIds set for this original assetNames?
-    // are the nft already created? (only delete the objects that are already created)
-    // note: bucket dir must match collectionname/id
+
+    const collectionNfts = await this.prismaService.nft.findMany({
+      where: { collectionId },
+      select: { tokenId: true },
+    })
+    // new ones need creation and uploading to aws
+    // duplicate ones need to be deleted from aws and re-uploaded
+    const [duplicateTokenIds, newTokenIds] = partition(
+      requestAssetIds,
+      (currentAssetNft) => {
+        const idxInStore = collectionNfts.findIndex(
+          ({ tokenId }) => tokenId === +currentAssetNft,
+        )
+        const itExistsInDb = idxInStore !== -1
+        return itExistsInDb
+      },
+    )
     return 'processing images'
   }
 }
